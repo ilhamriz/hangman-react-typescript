a388e4f98d84e07a10fd7443dbc6cc735bc7ff9c
ilhamriz <milhamrizky7@gmail.com>
2023-01-26 21:27:23 +0700

    add KeyboardEvent

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { HangmanDrawing, HangmanWord, Keyboard } from "./Components";
 import words from "./wordList.json";
@@ -8,10 +8,33 @@ function App() {
     return words[Math.floor(Math.random() * words.length)];
   });
 
-  const [guessLetters, setguessLetters] = useState<string[]>([]);
-  const incorrectLetters = guessLetters.filter(letter => !wordToGuess.includes(letter))
+  const [guessLetters, setGuessLetters] = useState<string[]>([]);
+  const incorrectLetters = guessLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
 
-  // console.log(wordToGuess);
+  const addGuessLetters = (letter: string) => {
+    if (guessLetters.includes(letter)) return;
+
+    setGuessLetters((currentLetters) => [...currentLetters, letter]);
+  };
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+
+      if (!key.match(/^[a-z]$/g)) return;
+
+      e.preventDefault();
+      addGuessLetters(key);
+    };
+
+    document.addEventListener("keypress", handler);
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div
@@ -26,7 +49,7 @@ function App() {
     >
       <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose/Win</div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
-      <HangmanWord />
+      <HangmanWord guessLetters={guessLetters} wordToGuess={wordToGuess} />
       <div style={{ alignSelf: "stretch" }}>
         <Keyboard />
       </div>
